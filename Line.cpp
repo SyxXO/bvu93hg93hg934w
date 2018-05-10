7d274fa31eee3134828c209bb92c0d91b7a1cb70
SyxXO <38927456+SyxXO@users.noreply.github.com>
2018-05-11 09:02:46 +1200

    Update Line.cpp

--- Line.cpp
+++ Line.cpp
@@ -11,8 +11,8 @@ extern "C" int read_analog(int ch_adc);
 extern "C" int take_picture();
 extern "C" char get_pixel(int row, int col, int color);
 
-// Bunch of code, need to divide for individual work. Does not include line
-// following code and does not include wall detectors, yet.
+    // Bunch of code, need to divide for individual work. Does not include line
+    // following code and does not include wall detectors, yet.
 
 int Average(); //to calculate
 int turnLeft();
@@ -23,9 +23,10 @@ int wallDetector();
 int defaultSpeed (20);
 void lineRunner();
 
-// sets up everything to ensure it is working.
+    // sets up everything to ensure it is working.
 
-int main(){
+int main()
+    {
     init();
     printf("ADC mean: %d/n", Average);
     turnLeft(1);
@@ -34,36 +35,37 @@ int main(){
     wallDetector();
     stop();
     return 0;
-}
+    }
 
-//kill program. Only needs to be used to test we actually have something.
+    //kill program. Only needs to be used to test we actually have something.
 
-int Average(){
+int Average()
+    {
     int track();
     int Average;
 
     // read analog input
-
-    for(int c = 0; c<10; c++){
+    for(int c = 0; c<10; c++)
+        {
         track += read_analog(0);
         sleep(0,5);
         return Average;
+        }
     }
-}
 
-// line following code - maybe?
-// Basically we need a control loop to maintain an error value (eVal) to follow a line.
-// Outcome - we need to constantly calculate error values and apply the scaled error to our motors (turns based on what it sees)
-// threshhold is always the middle value (1-3 = 2) whic is what must be maintained.
-// Must have error, proprotional term (KP) derivative (KD) integral (KI) scaled value
-// (Speed with Error) and the applied value (Scale value to motor.)
-// For this the actual line followed would be the edges of the white line, not the centre.
+    // line following code - maybe?
+    // Basically we need a control loop to maintain an error value (eVal) to follow a line.
+    // Outcome - we need to constantly calculate error values and apply the scaled error to our motors (turns based on what it sees)
+    // threshhold is always the middle value (1-3 = 2) whic is what must be maintained.
+    // Must have error, proprotional term (KP) kDer (KD) kInt (KI) scaled value
+    // (Speed with Error) and the applied value (Scale value to motor.)
+    // For this the actual line followed would be the edges of the white line, not the centre.
 
-void lineRunner(){
+void lineRunner()
+    {
     int eVal();   //determines the current error of what is shown.
     int previousVal();  //tracks the previous error of what was shown.
     int difference();   // calculates the difference between previous and current.
-
     int error(); // tracks the total error (averages are cool) Sensor readings are key for errors.
     int pixelTot(); // pretty much the foundation, differentiates colours.
 
@@ -71,10 +73,10 @@ void lineRunner(){
     clock_t start;
     clock_t now;
 
-    float derivative = 0; // constant derivative of errors.
+    float kDer = 0; // constant derivative.
     int derSig = 0; // used to scale motors.
 
-    float integral = 0; // constant integral of errors.
+    float kInt = 0; // constant integral.
     int intSig = 0; // used to scale motors.
 
     float kp = 0.1; // probably have to test this out, this changes based on a lot of factors.
@@ -82,48 +84,54 @@ void lineRunner(){
 
     int defaultSpeed = 30;
 
-    // This part actually follows the line (hopefully) based on all the previously set things.
+        // This part actually follows the line (hopefully) based on all the previously set things.
 
-    while(true){ // will have to eventually break this to turn it off once the quadrant has been completed and the sensors detect walls.
+    while(true)
+        { // will have to eventually break this to turn it off once the quadrant has been completed and the sensors detect walls.
+            start = clock();
+            take_picture();
+            eVal = 0; // keep this 0
+            pSig = 0; // it's a constant, keep it 0.
+            pixelTot = 0;
 
-        start = clock();
-        take_picture();
-        eVal = 0; // keep this 0
-        pSig = 0; // it's a constant, keep it 0.
-        pixelTot = 0;
-
-        for(int count = 0; count < 320; count++){
+            for(int count = 0; count < 320; count++)
+            {
             int noise = get_pixel(count, 120, 3); // Gives pixel brightness, handles the noise.
 
-            if(noise < 120){
-                noise = 0; // Always reset, it's a loop.
+            if(noise < 120)
+                {
+                    noise = 0; // Always reset, it's a loop.
+                }
+                else
+                {
+                    noise = 1; // To follow the line.
+                    pixelTot += 1;
+                }
+
+                eVal += (count - 160) * noise; // Equates to 0, ensuring the edges of the line are followed. I hope.
             }
-            else {
-                noise = 1; // To follow the line.
-                pixelTot += 1;
+            // if there is no walls, only lines - aka quadrant whatever.
+
+            if (eVal < 500 && eval > -500 && pixelTot > 300)
+            {
+                return; // we out boiz.
             }
-            eVal += (count - 160) * noise; // Equates to 0, ensuring the edges of the line are followed. I hope.
-        }
-        // if there is no walls, only lines - aka quadrant whatever.
 
-        if (eVal < 500 && eval > -500 && pixelTot > 300){
-            return; // we out boiz.
-        }
-        now = clock();
-        ePeriod = ((double)(now - start)) / CLOCKS_PER_SEC;
+            now = clock();
+            ePeriod = ((double)(now - start)) / CLOCKS_PER_SEC;
 
-        difference = eVal - previousVal;
-        derSig = (difference/ePeriod) * derivative; // constant
+            difference = eVal - previousVal;
+            derSig = (difference/ePeriod) * kDer; // constant
 
-        previousVal = eVal;
-        error += eVal; //Sums error.
+            previousVal = eVal;
+            error += eVal; //Sums error.
 
-        pSig = eVal*kp;
-        intSig = (error * integral);
+            pSig = eVal*kp;
+            intSig = (error * kInt);
 
-        //change the speeds, based on the scaling of everything prior.
+            //change the speeds, based on the scaling of everything prior.
 
-        set_motor(2, defaultSpeed - pSig - intSig + derSig);
-        set_motor(1, defaultSpeed + pSig - intSig -derSig);
+            set_motor(2, defaultSpeed - pSig - intSig + derSig);
+            set_motor(1, defaultSpeed + pSig - intSig -derSig);
+            }
     }
-}
